ec61317a4c6a51d85630a69898cddce2c23daeae
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-29 13:52:33 -0600

    Fix download link and download for macos

--- freepascal/build.sh
+++ freepascal/build.sh
@@ -4,3 +4,14 @@ set -e
 set -x
 
 echo $pwd
+
+wget https://downloads.sourceforge.net/project/freepascal/Bootstrap/3.0.0/x86_64-macosx-10.7-ppcx64.tar.bz2
+tar -xf x86_64-macosx-10.7-ppcx64.tar.bz2
+mkdir -p fpc_bootstrap
+mv ./ppcx64 ./fpc_bootstrap/ppcx64
+
+export FPC_COMPILER="./fpc_bootstrap/ppcx64"
+
+make build PP=${FPC_COMPILER}
+make install PP=${FPC_COMPILER} PREFIX=${PREFIX}
+
