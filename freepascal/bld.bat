c24918837b3c3f07777085798225d08366617780
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2018-07-29 18:46:57 -0600

    Add Windows build

--- freepascal/bld.bat
+++ freepascal/bld.bat
@@ -7,4 +7,19 @@ if %PY3K% equ 1 (
   set PYTHON_VERSION="%PY_VER%"
 )
 
-popd
+wget ftp://ftp.freepascal.org/pub/fpc/dist/3.0.4/bootstrap/i386-win32-ppc386.zip
+7z i386-win32-ppc386.zip
+
+@echo on
+set myroot=%LIBRARY_PREFIX%
+set myFPC=%myroot%\fpc\3.0.4
+set mybinutils=%myroot%\binutils
+set PATH=%mybinutils%\i386-win32;%myFPC%\bin\i386-win32;%PATH%
+
+dir /s /b /o:gn
+
+rd /s /q  examples
+make distclean all install INSTALL_PREFIX=%myFPC% PP=.\ppc386.exe DATA2INC=%myFPC%\utils\data2inc.exe
+cd /d %myFPC%\bin\i386-win32
+fpcmkcfg -d basepath=%myFPC% -o .\fpc.cfg
+copy fpc.exe %mybinutils%\i386-win32
